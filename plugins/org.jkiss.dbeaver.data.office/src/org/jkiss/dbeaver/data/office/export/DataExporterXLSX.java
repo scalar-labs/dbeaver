2edb1282e2050b68b9457dc9151367cadb52f6a7
yu2scalar <yu2scalar@gmail.com>
2026-01-27 21:15:33 +0900

    fix ScalarDB 3.17.1 driver integration and XLSX export
       - Fix Guava version conflict by declaring guava:32.1.3-jre first in
         plugin.xml to override older transitive dependencies (30.1, 19.0)
       - Use scalardb-cluster-java-client-sdk per official documentation
       - Fix POI cell style cloning using createCellStyle() + cloneStyleFrom()
       - Update P2 repository URL to repo.dbeaver.net

--- plugins/org.jkiss.dbeaver.data.office/src/org/jkiss/dbeaver/data/office/export/DataExporterXLSX.java
+++ plugins/org.jkiss.dbeaver.data.office/src/org/jkiss/dbeaver/data/office/export/DataExporterXLSX.java
@@ -543,7 +543,8 @@ public class DataExporterXLSX extends StreamExporterAbstract implements IAppenda
 
             if (bg != null) {
                 // Setting the foreground color sets the background color. Is this a bug/feature of POI?
-                final XSSFCellStyle style = (XSSFCellStyle) this.style.clone();
+                final XSSFCellStyle style = (XSSFCellStyle) wb.createCellStyle();
+                style.cloneStyleFrom(this.style);
                 style.setFillForegroundColor(new XSSFColor(asColor(bg), new DefaultIndexedColorMap()));
                 style.setFillPattern(FillPatternType.SOLID_FOREGROUND);
 
